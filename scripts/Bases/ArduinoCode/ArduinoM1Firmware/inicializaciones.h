33b9705dc36b7c31e90fa4dbc8756e38566cb757
lucasbaldezzari <lmbaldezzari@gmail.com>
2021-09-28 19:53:15 -0300

    Se borran lineas innecesarias

--- scripts/Bases/ArduinoCode/ArduinoM1Firmware/inicializaciones.h
+++ scripts/Bases/ArduinoCode/ArduinoM1Firmware/inicializaciones.h
@@ -12,35 +12,9 @@
 #define PRE_SCALER      64UL
 
 #include <avr/io.h>      // Contains all the I/O Register Macros
-#include <util/delay.h>  // Generates a Blocking Delay
-#include <avr/interrupt.h> // Contains all interrupt vectors
-
-#define USART_BAUDRATE 19200 // Desired Baud Rate
-#define BAUD_PRESCALER (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
-
-#define ASYNCHRONOUS (0<<UMSEL00) // USART Mode Selection
-
-#define DISABLED    (0<<UPM00)
-#define EVEN_PARITY (2<<UPM00)
-#define ODD_PARITY  (3<<UPM00)
-#define PARITY_MODE  DISABLED // USART Parity Bit Selection
-
-#define ONE_BIT (0<<USBS0)
-#define TWO_BIT (1<<USBS0)
-#define STOP_BIT ONE_BIT      // USART Stop Bit Selection
-
-#define FIVE_BIT  (0<<UCSZ00)
-#define SIX_BIT   (1<<UCSZ00)
-#define SEVEN_BIT (2<<UCSZ00)
-#define EIGHT_BIT (3<<UCSZ00)
-#define DATA_BIT   EIGHT_BIT  // USART Data Bit Selection
-
-#define RX_COMPLETE_INTERRUPT         (1<<RXCIE0)
-#define DATA_REGISTER_EMPTY_INTERRUPT (1<<UDRIE0)
 
 void iniTimer0(int frecTimer);
 void iniTimer2();
-void iniUART();
 
 void iniTimer0(int frecTimer)
 {
